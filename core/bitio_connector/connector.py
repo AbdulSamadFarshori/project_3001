77a2ddf6d7ec683e62dfb8e393e25a574b3b793c
abdul samad <sumir40@gmail.com>
2022-08-17 13:22:40 +0530

    api_call

--- core/bitio_connector/connector.py
+++ core/bitio_connector/connector.py
@@ -1,41 +1,56 @@
-from sqlalchemy import create_engine
-import pandas as pd
+import requests
 
 class BitioConnector(object):
     
     def __init__(self):
-        self.string = "postgresql://sumir40:v2_3tRg3_44jAdLpQamRykebkeyeTwu2@db.bit.io/sumir40/project_3001"
+        self.string = "v2_3tRkM_JwL4zBbvrRxaunZnkN4bHJ8"
+        self.database = "sumir40/project_3001"
 
-    def engine(self):
-        eng=create_engine(self.string, isolation_level="AUTOCOMMIT")
-        return eng 
+    def engine(self, query):
+        headers = {"Authorization": "Bearer {}".format(self.string),
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    }
+        url = "https://api.bit.io/v2beta/query"
 
-    def callback(self, query):
-        with self.engine().connect() as conn:
-            result = conn.execute("{}".format(query))
-            return result
+        data = {"query_string":"{}".format(query), "database_name":"{}".format(self.database)}
+
+        response = requests.post(url, json=data, headers=headers)
+        return response.json() 
+
+
+        
 
     def get_main_data(self):
         query_anxiety = "SELECT * FROM api_main_data WHERE heading = 'Anxiety Disorders' LIMIT 2000;"
         query_depression = "SELECT * FROM api_main_data WHERE heading = 'Depression' LIMIT 2000;"
-        anxiety_data = self.callback(query_anxiety)
-        depression_data = self.callback(query_depression)
+        anxiety_data = self.engine(query_anxiety)
+        depression_data = self.engine(query_depression)
         return anxiety_data, depression_data
 
     # get_data_between 
 
     def convert_to_dict(self):
-        
+        final_dict = []
         anxiety_object, depression_object = self.get_main_data()
-        _key = anxiety_object.keys()
-        anx_df = pd.DataFrame(anxiety_object.all())
-        dep_df = pd.DataFrame(depression_object.all())
-        all_join = [anx_df, dep_df]
-        final_df = pd.concat(all_join)
-        data_dict = final_df.to_dict("records")
-        return data_dict
+        column = ["id", "heading", "sub_heading", "main_problem", "author_name"]
+        for point in anxiety_object["data"]:
+            temp = {}
+            for d in range(len(point)):
+                temp[column[d]] = point[d]
+            final_dict.append(temp)
+
+        for row in depression_object["data"]:
+            temp2 = {}
+            for d in range(len(row)):
+                temp2[column[d]] = row[d]
+            final_dict.append(temp2)
+        
+        return final_dict
+        
 
 
 connects = BitioConnector()
-data = connects.convert_to_dict() 
+data = connects.convert_to_dict()
+ 
 
