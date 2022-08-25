3590b672781ce8aa798cf445e9c21e7e2a046d0c
abdul samad <sumir40@gmail.com>
2022-08-25 14:38:05 +0530

    temp on

--- api/admin.py
+++ api/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from .models import main_data, response, keywords, CompletedCase
+# from .models import main_data, response, keywords, CompletedCase
  
-@admin.register(main_data)
-class MainDataAdmin(admin.ModelAdmin):
-  list_display = ['id', 'heading', 'sub_heading', 'main_problem' ,'author_name']
+# @admin.register(main_data)
+# class MainDataAdmin(admin.ModelAdmin):
+#   list_display = ['id', 'heading', 'sub_heading', 'main_problem' ,'author_name']
 
 
-@admin.register(response)
-class ResponseAdmin(admin.ModelAdmin):
-  list_display = ['case_id', 'counsellor', 'petient_asking', 'relavent_score' ,'summary', 'reply']
+# @admin.register(response)
+# class ResponseAdmin(admin.ModelAdmin):
+#   list_display = ['case_id', 'counsellor', 'petient_asking', 'relavent_score' ,'summary', 'reply']
 
 
-@admin.register(keywords)
-class KeywordAdmin(admin.ModelAdmin):
-  list_display = ['case_id', 'keyword']
+# @admin.register(keywords)
+# class KeywordAdmin(admin.ModelAdmin):
+#   list_display = ['case_id', 'keyword']
 
 
-@admin.register(CompletedCase)
-class CompleteCasesAdmin(admin.ModelAdmin):
-  list_display = ['case_id']
+# @admin.register(CompletedCase)
+# class CompleteCasesAdmin(admin.ModelAdmin):
+#   list_display = ['case_id']
