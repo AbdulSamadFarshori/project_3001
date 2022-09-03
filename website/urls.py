f327800fe14959406a328c22706a60b28a9f7bb8
abdul samad <sumir40@gmail.com>
2022-09-03 10:03:53 +0530

    change in url

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import HomeView, LoginView, MainView, LabelView
+from .views import HomeView, LoginView, MainView
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('login', LoginView.as_view(), name="login"),
     path('main/<var>', MainView.as_view(), name="main"),
-    path('label/value', LabelView.as_view(), name="main")
     ]
