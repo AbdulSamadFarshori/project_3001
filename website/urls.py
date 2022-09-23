ae80e10040d8d27d4c4a1b7fbd700e5146e40d9d
abdul samad <sumir40@gmail.com>
2022-09-23 14:48:38 +0530

    add completed case

--- website/urls.py
+++ website/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('login', LoginView.as_view(), name="login"),
+    path('login', LoginView.as_view(template_name='website/login.html'), name="login"),
     path('main', MainView.as_view(), name="main"),
     path('completed-cases', CompletedCasesPageTemplate.as_view(), name="completed"),
     path('not-completed-cases', NotCompletedCasesPageTemplate.as_view(), name="not-completed"),
