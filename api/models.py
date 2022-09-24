c3fcad7447daac4918b3e375c2e999adee91d29e
abdul samad <sumir40@gmail.com>
2022-09-24 17:49:42 +0530

    add completed case

--- api/models.py
+++ api/models.py
@@ -169,8 +169,8 @@ class Effect(models.Model):
 		verbose_name_plural = "Effect"
 
 class JWTToken(models.Model):
-	refresh_token = CharField(max_length=500)
-	access_token = CharField(max_length=500)
+	refresh_token = models.CharField(max_length=500)
+	access_token = models.CharField(max_length=500)
 
 	def __str__(self):
 		return self.refresh_token
