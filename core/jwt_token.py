a5166d55ec7b67ea13d378f0f50a0d9a2427cd73
abdul samad <sumir40@gmail.com>
2022-09-24 19:18:34 +0530

    full new system

--- core/jwt_token.py
+++ core/jwt_token.py
@@ -1,6 +1,6 @@
 from api.models import JWTToken
 
 def get_access_token():
-	token = JWTToken.objects.get(id=1).first()
+	token = JWTToken.objects.get(id=1)
 	access_token = token.access_token
 	return access_token
