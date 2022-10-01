5275dd37a46ba5d4f1cbb4d75c2b12c7ebd8b803
abdul samad <sumir40@gmail.com>
2022-10-01 15:17:05 +0530

    full new system

--- core/text_to_sentence.py
+++ core/text_to_sentence.py
@@ -9,10 +9,6 @@ nltk.download('punkt')
 class TextToSentence(object):
 
 	def __init__(self, text):
-		text = re.sub(r"..", ".", text)
-		text = re.sub(r"...", ".", text)
-		text = re.sub(r"....", ".", text)
-		text = re.sub(r".....", ".", text)
 		self.text = text
 
 	def model(self):
@@ -21,9 +17,23 @@ class TextToSentence(object):
 		
 		return Model
 
+	def remove_stops(self):
+		words = []
+		for st in self.text:
+			word = st
+			stops = 0
+			for ex_st in st:
+				if ex_st == ".":
+					stops += 1
+			if stops > 1:
+				word = '. '
+			words.append(word)
+		text = " ".join(words)
+		return text
+
 	def text_to_sentence_list(self):
 		
-		output = self.model()._tokenize_text_to_sentences(self.text)
+		output = self.model()._tokenize_text_to_sentences(self.remove_stops)
 
 		return output
 
